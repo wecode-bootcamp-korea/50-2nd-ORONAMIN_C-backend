e6b6b0a1605d4186176d47a8dd936f0e6ddd5ae7
juhyunju <2juhyunju@naver.com>
2023-10-23 19:58:49 +0900

    feat: settings

---
+++ app.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const express = require('express')
+const routes = require("./routes");
+
+const app = express()
+app.use(express.json())
+app.use(routes)
+
+app.get('/',(req,res) => {
+    res.status(200).json({
+        message: "hello"
+    })
+})
+
+const server = http.createServer(app)
+
+const start = async () => { // 서버를 시작하는 함수입니다.
+    try {
+      server.listen(8000, () => console.log(`Server is listening on 8000`))
+      
+    } catch (err) { 
+      console.error(err)
+    }
+  }
+
+start()
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+//routes/index.js
+const express = require("express");
+const router = express.Router();
+
+
+
+module.exports = router;
