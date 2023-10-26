32546e5ba240fad8f2853843242f5692a27fbbd7
Yeong Ki Hong <yeongkihong@Yeongui-MacBookAir.local>
2023-10-26 15:25:37 +0900

    관리자 계정 권한 추가

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv")
 
 dotenv.config()
 
-const makeToken = async(userId, userEmail, userNickname) => {
-    return jwt.sign({id:userId,email:userEmail,nickname:userNickname},process.env.TYPEORM_SECRETKEY,{expiresIn:60*60})
+const makeToken = async(userId, userEmail, userNickname, admin_status) => {
+    return jwt.sign({id:userId,email:userEmail,nickname:userNickname, status : admin_status},process.env.TYPEORM_SECRETKEY,{expiresIn:1})
 }
 
 const verifyToken = async(token) => {
