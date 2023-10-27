077636fdf5eeb6369ad8518599cba612b0d5d258
Yeong Ki Hong <yeongkihong@Yeongui-MacBookAir.local>
2023-10-27 13:50:26 +0900

    에러핸들링 유틸 추가, SQL조건별 에러 캐치 추가

---
+++ util/sqlResult.js
@@ -0,0 +1,11 @@
+const sqlResult = (result) =>{
+    console.log("SQL 데이터 변동 결과")
+    console.log("-----------------")
+    if(result.insertId>0){console.log("추가 된 행 : 1행")}
+    console.log("영향을 받은 행 : ", result.affectedRows, "행" )
+    console.log("수정/삭제 된 행 : ", result.changedRows, "행" )
+    if (result.serverStatus=2){console.log("서버 상태 : 정상")}
+    else {console.log("SQL 서버가 정상작동중인지 확인이 필요합니다.")}
+}
+
+module.exports = { sqlResult }
