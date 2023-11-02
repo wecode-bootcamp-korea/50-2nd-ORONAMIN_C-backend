ce0f02b620068c92882d5bc00681a6b8666ab6e0
juhyunju <2juhyunju@naver.com>
2023-11-02 15:57:56 +0900

    refactor: modify product

--- controller/productController.js
+++ controller/productController.js
@@ -1,102 +1,121 @@
-const productService = require('../services/productService')
-const middleJwt = require('../middleware/jwt');
+const productService = require("../services/productService");
+const middleJwt = require("../middleware/jwt");
 
-const productAll = async(req,res) =>{
-    try{
-        const {brandName,scentName} = req.query
-        const products = await productService.getProducts(brandName,scentName)
-        res.status(200).json({products})
-    }catch(err){
-        res.status(500 || statusCode).json({message: err.message})
-    }
-}
+const productAll = async (req, res) => {
+  try {
+    const { brandName, scentName } = req.query;
+    const products = await productService.getProducts(brandName, scentName);
+    res.status(200).json({ products });
+  } catch (err) {
+    res.status(statusCode || 500).json({ message: err.message });
+  }
+};
 
-const getProduct = async(req,res) => {
-    const id = req.params.id
-    try{
-        const product = await productService.getProduct(id)
-        res.status(200).json({product})
-    }catch(err){
-        res.status(err.statusCode).json({message: err.message})
+const getProduct = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const product = await productService.getProduct(id);
+    res.status(200).json({ product });
+  } catch (err) {
+    res.status(err.statusCode).json({ message: err.message });
+  }
+};
+const createProduct = async (req, res) => {
+  const { productName, price, description, brandId, scentId, imageId } =req.body;
+  const token = req.headers.authorization.substr(7);
+  const decodedToken = await middleJwt.verifyToken(token);
+  try {
+    if (!productName ||!price ||!description ||!brandId ||!scentId ||!imageId) {
+      const inputKeyError = new Error();
+      inputKeyError.statusCode = 400;
+      inputKeyError.message = "INPUT_KEY_ERROR";
+      throw inputKeyError;
     }
-}
-const createProduct = async(req,res) =>{
-    const {productName,price,description,brandId,scentId,imageId} = req.body
-    const token = req.headers.authorization.substr(7)
-    const decodedToken = await middleJwt.verifyToken(token)
-    try{
-        if(!productName||!price||!description||!brandId||!scentId||!imageId){
-            const inputKeyError = new Error()
-            inputKeyError.statusCode = 400
-            inputKeyError.message = 'INPUT_KEY_ERROR'
-            throw inputKeyError
-        }
-        if(token){
-            const userType = {
-                ADMIN:1
-            }
-            if(decodedToken.status === userType.ADMIN){
-                await productService.createProduct(productName,price,description,brandId,scentId,imageId)
-                res.status(200).json({message: "successfully created"})
-            }else{
-                const err = new Error()
-                err.statusCode = 401
-                err.message = "Permission Denied"
-                throw err;
-            }
-        }
-    }catch(err){
-        res.status(404|| statusCode).json({message: err.message})
+    if (token) {
+      const userType = {
+        ADMIN: 1,
+      };
+      if (decodedToken.status === userType.ADMIN) {
+        await productService.createProduct(
+          productName,
+          price,
+          description,
+          brandId,
+          scentId,
+          imageId
+        );
+        res.status(200).json({ message: "successfully created" });
+      } else {
+        const err = new Error();
+        err.statusCode = 401;
+        err.message = "Permission Denied";
+        throw err;
+      }
     }
-}
-const updateProduct = async (req,res) =>{
-    const productId = req.params.productId
-    const {productName,price,description,brandId,scentId,imageId} = req.body
-    const token = req.headers.authorization.substr(7)
-    const decodedToken = await middleJwt.verifyToken(token)
-    try{
-        if(token){
-            const userType = {
-                ADMIN:1
-            }
-            if(decodedToken.status === userType.ADMIN){
-                await productService.updateProduct(productId,productName,price,description,brandId,scentId,imageId)
-                res.status(200).json({message: "successfully updated"})
-            }else{
-                const err = new Error()
-                err.statusCode = 401
-                err.message = "Permission Denied"
-                throw err;
-            }
-        }
-    }catch(err){
-        res.status(500 || statusCode).json({message: err.message})
+  } catch (err) {
+    res.status(404 || statusCode).json({ message: err.message });
+  }
+};
+const updateProduct = async (req, res) => {
+  const productId = req.params.productId;
+  const { productName, price, description, brandId, scentId, imageId } =
+    req.body;
+  const token = req.headers.authorization.substr(7);
+  const decodedToken = await middleJwt.verifyToken(token);
+  try {
+    if (token) {
+      const userType = {
+        ADMIN: 1,
+      };
+      if (decodedToken.status === userType.ADMIN) {
+        await productService.updateProduct(
+          productId,
+          productName,
+          price,
+          description,
+          brandId,
+          scentId,
+          imageId
+        );
+        res.status(200).json({ message: "successfully updated" });
+      } else {
+        const err = new Error();
+        err.statusCode = 401;
+        err.message = "Permission Denied";
+        throw err;
+      }
     }
-}
-const deleteProduct = async(req,res)=>{
-    const id = req.params.id
-    const token = req.headers.authorization.substr(7)
-    const decodedToken = await middleJwt.verifyToken(token)
-    try{
-        if(token){
-            const userType = {
-                ADMIN:1
-            }
-            if(decodedToken.status === userType.ADMIN)
-            await productService.deleteProduct(id)
-            res.status(204).json({message:"삭제되었습니다."})
-        }else {
-            const err = new Error()
-            err.statusCode = 401
-            err.message = "Permission Denied"
-            throw err;
-        }
-    }catch(err){
-        res.status(404||statusCode).json({message: err.message})
+  } catch (err) {
+    res.status(500 || statusCode).json({ message: err.message });
+  }
+};
+const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+  const token = req.headers.authorization.substr(7);
+  const decodedToken = await middleJwt.verifyToken(token);
+  try {
+    if (token) {
+      const userType = {
+        ADMIN: 1,
+      };
+      if (decodedToken.status === userType.ADMIN)
+        await productService.deleteProduct(id);
+      res.status(204).json({ message: "삭제되었습니다." });
+    } else {
+      const err = new Error();
+      err.statusCode = 401;
+      err.message = "Permission Denied";
+      throw err;
     }
-}
-
+  } catch (err) {
+    res.status(404 || statusCode).json({ message: err.message });
+  }
+};
 
 module.exports = {
-    productAll,getProduct,createProduct,deleteProduct,updateProduct
-}
+  productAll,
+  getProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+};
--- services/productService.js
+++ services/productService.js
@@ -1,46 +1,46 @@
-const productDao = require('../models/productDao')
-const jwt = require('../middleware/jwt')
+const productDao = require("../models/productDao");
 
-const getProducts = async(brandName,scentName) =>{
-    const productAll = await productDao.getProducts(brandName,scentName)
-    return productAll
-}
+const getProducts = async (brandName, scentName) => {
+  const productAll = await productDao.getProducts(brandName, scentName);
+  return productAll;
+};
 
-const getProduct = async(id) =>{
-    const product = await productDao.getProduct(id)
-    if(product.length === 0){
-        const err = new Error()
-        err.statusCode = 400
-        err.message = "없는 상품입니다."
-        throw err;
-    }
-    return product
-} 
+const getProduct = async (id) => {
+  const product = await productDao.getProduct(id);
+  if (product.length === 0) {
+    const err = new Error();
+    err.statusCode = 400;
+    err.message = "없는 상품입니다.";
+    throw err;
+  }
+  return product;
+};
 
-const createProduct = async(productName,price,description,brandId,scentId,imageId) =>{
-    const product = await productDao.createProduct(productName,price,description,brandId,scentId,imageId)
-    return product
-}
+const createProduct = async (productName,price,description,brandId,scentId,imageId) => {
+  const product = await productDao.createProduct(productName,price,description,brandId,scentId,imageId);
+  return product;
+};
 
-const updateProduct = async(productId,productName,price,description,brandId,scentId,imageId)=>{
-    const getProduct = await productDao.getProduct(productId)
-    if(getProduct.length === 0){
-        const err = new Error()
-        err.statusCode = 404
-        err.message = "없는 상품입니다."
-        throw err;
-    }
-    const product = await productDao.updateProduct(productId,productName,price,description,brandId,scentId,imageId)
-    return product 
-
-}
-const deleteProduct = async(id) =>{
-    const product = await productDao.deleteProduct(id)
-    return product
-} 
-
-
+const updateProduct = async (productId,productName,price,description,brandId,scentId,imageId) => {
+  const getProduct = await productDao.getProduct(productId);
+  if (getProduct.length === 0) {
+    const err = new Error();
+    err.statusCode = 404;
+    err.message = "없는 상품입니다.";
+    throw err;
+  }
+  const product = await productDao.updateProduct(productId,productName,price,description,brandId,scentId,imageId);
+  return product;
+};
+const deleteProduct = async (id) => {
+  const product = await productDao.deleteProduct(id);
+  return product;
+};
 
 module.exports = {
-    getProducts,getProduct,createProduct,deleteProduct,updateProduct
-}
+  getProducts,
+  getProduct,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+};
